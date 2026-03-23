baaaa9de91c660baccc56a7067f905f4fd0de524
Kemal Selçuk <kemal.selcuk.w@gmail.com>
2026-03-23 15:54:22 +0300

    dependency fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,9 +8,7 @@ plugins {
 
 android {
     namespace = "com.example.newsapp"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.newsapp"
